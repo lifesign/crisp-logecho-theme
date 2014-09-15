f7a56cb8206e274c3de56ae75bf842c15284f82d
lifesign <leocloudy@gmail.com>
2014-09-15 19:23:42 +0800

    add logo and improve loading disqus

---
+++ _theme/base.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('body').on('click', '.show-commend', function() {
+        var ds_loaded = false;
+        window.disqus_shortname = $('.show-commend').attr('name');
+
+        $.ajax({
+            type: "GET",
+            url: "http://" + disqus_shortname + ".disqus.com/embed.js",
+            dataType: "script",
+            cache: true
+        });
+    });
+});
